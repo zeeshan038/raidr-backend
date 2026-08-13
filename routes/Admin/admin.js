0330c7f492e5a0c8439fafa8c1b3960c4bc7f5a3
Zeeshan <hankhan123623@gmail.com>
2026-08-13 22:52:17 +0500

    Fix uWebSockets crash by downgrading to v20.44.0

--- routes/Admin/admin.js
+++ routes/Admin/admin.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import { registerAdmin, loginAdmin } from '../../controllers/Admin/admin.js';
+import { verifyAdmin } from '../../middlewares/verifyAdmin.js';
 
 router.post('/register', registerAdmin);
 router.post('/login', loginAdmin);
